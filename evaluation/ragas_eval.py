ddbdbe35d68251c99f0c5f89e742ea49ca8b6457
Tirath singh <tirath2874@gmail.com>
2026-06-23 09:59:16 +0530

    test

--- evaluation/ragas_eval.py
+++ evaluation/ragas_eval.py
@@ -1,62 +1,47 @@
 import json
-
 from datasets import Dataset
 
 from rag import rag_answer
 
 from ragas import evaluate
-
 from ragas.metrics import (
-    faithfulness,
-    answer_relevancy,
-    context_precision
+    Faithfulness,
+    ResponseRelevancy,
 )
 
-with open("evaluation/test_dataset.json") as f:
+with open("evaluation/test_dataset.json", "r", encoding="utf-8") as f:
     tests = json.load(f)
 
-questions=[]
-answers=[]
-contexts=[]
-ground_truths=[]
+questions = []
+answers = []
+contexts = []
+ground_truths = []
 
 for row in tests:
+    result = rag_answer(row["question"])
 
-    result = rag_answer(
-        row["question"]
-    )
+    questions.append(row["question"])
+    answers.append(result["answer"])
 
-    questions.append(
-        row["question"]
-    )
+    contexts.append([result["context"]])
 
-    answers.append(
-        result["answer"]
-    )
-
-    contexts.append(
-        [result["context"]]
-    )
-
-    ground_truths.append(
-        row["ground_truth"]
-    )
+    ground_truths.append(row["ground_truth"])
 
 dataset = Dataset.from_dict(
-{
-    "question":questions,
-    "answer":answers,
-    "contexts":contexts,
-    "ground_truth":ground_truths
-})
+    {
+        "user_input": questions,
+        "response": answers,
+        "retrieved_contexts": contexts,
+        "reference": ground_truths,
+    }
+)
 
 result = evaluate(
     dataset,
     metrics=[
-        faithfulness,
-        answer_relevancy,
-        context_precision
-    ]
+        Faithfulness(),
+        ResponseRelevancy(),
+    ],
 )
 
 print(result)
